53a4d107cae811df2092ae1513a116770550c50f
Rishi Dewan <rishidewan33@gmail.com>
2011-06-03 14:05:12 -0700

    Header file for the basic tests

--- basicThreadTests.h
+++ basicThreadTests.h
@@ -1,7 +1,7 @@
 #ifndef _BASIC_THREAD_TESTS_H_
 #define _BASIC_THREAD_TESTS_H_
 void basicThreadTests();
-void grandFinale();
+void grandFinale(); 
 void preemptiveTests();
 void lockTests();
 void condTests();
